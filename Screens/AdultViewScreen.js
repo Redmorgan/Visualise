b34df5ed364869d9ea1c6a2298442754d3e1af70
WilliamWernick <william.wernick@aucoda.com>
2022-03-01 12:30:15 +0000

    feat(update): Basic update functionality working, as well as page reloading

--- Screens/AdultViewScreen.js
+++ Screens/AdultViewScreen.js
@@ -44,19 +44,28 @@ const AdultViewScreen = ({ navigation }) => {
                     height:65}
                 }}>
 
-                <Tab.Screen name="Calendar" component={CalendarComponent} options={{
-                    tabBarIcon:({color}) =>(
-                        <Entypo name="calendar" size={24} color={color}/>
-                    ),
-                    tabBarItemStyle:{borderRightColor:"#ffffff", borderRightWidth:1}
-                }}/>
+                <Tab.Screen
+                    name="Calendar"
+                    component={CalendarComponent}
+                    options={{
+                        tabBarIcon:({color}) =>(
+                            <Entypo name="calendar" size={24} color={color}/>
+                        ),
+                        tabBarItemStyle:{borderRightColor:"#ffffff", borderRightWidth:1}
+                    }}
+                />
 
-                <Tab.Screen name="Today" component={TodayComponent} options={{
-                    tabBarIcon:({color}) =>(
-                        <AntDesign name="clockcircle" size={24} color={color} />
-                    ),
-                    tabBarItemStyle:{borderColor:"#ffffff", borderLeftWidth:1, borderRightWidth:1}
-                }}/>
+                <Tab.Screen
+                    name="Today"
+                    component={TodayComponent}
+                    options={{
+                        unmountOnBlur:true,
+                        tabBarIcon:({color}) =>(
+                            <AntDesign name="clockcircle" size={24} color={color} />
+                        ),
+                        tabBarItemStyle:{borderColor:"#ffffff", borderLeftWidth:1, borderRightWidth:1}
+                    }}
+                />
 
                 <Tab.Screen
                     name="Tasks"
@@ -66,7 +75,8 @@ const AdultViewScreen = ({ navigation }) => {
                             <FontAwesome name="cog" size={24} color={color} />
                         ),
                         tabBarItemStyle:{borderLeftColor:"#ffffff", borderLeftWidth:1}
-                }}/>
+                    }}
+                />
 
             </Tab.Navigator>
 
